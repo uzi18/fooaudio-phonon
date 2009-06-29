2f3e474a1b3c710ea0896faa9073951d314be4c2
Mateusz Marek <matthew@matthew.org.pl>
2009-06-30 00:01:47 +0200

    foo

--- src/fooaudio/fooaudioengine.hpp
+++ src/fooaudio/fooaudioengine.hpp
@@ -7,6 +7,8 @@
 #include "../include/foooutbuf.hpp"
 #include <pthread.h>
 
+#define PCM_BUF_SIZE		(32 * 1024)
+
 enum Request
 {
 	REQ_NOTHING,
@@ -21,6 +23,35 @@ enum State
 	STATE_PLAY,
 	STATE_STOP,
 	STATE_PAUSE
+};
+
+struct BitrateListNode
+{
+	BitrateListNode *next;
+	int time;
+	int bitrate;
+};
+
+struct BitrateList
+{
+	BitrateListNode *head;
+	BitrateListNode *tail;
+	pthread_mutex_t mutex;
+};
+
+struct Precache
+{
+	char *file; /* the file to precache */
+	char buf[2 * PCM_BUF_SIZE]; /* PCM buffer with precached data */
+	int bufFill;
+	int ok; /* 1 if precache succeed */
+	SoundParams soundParams; /* of the sound in the buffer */
+	struct decoder *f; /* decoder functions for precached file */
+	void *decoderData;
+	int running; /* if the precache thread is running */
+	pthread_t tid; /* tid of the precache thread */
+	BitrateList bitrateList;
+	int decodedTime; /* how much sound we decoded in seconds */
 };
 
 class FooAudioEngine
@@ -38,8 +69,10 @@ public:
 	int currPlaying;
 	void *playThread (void *);
 
-	void player (const char *, const char *, FooOutBuf *);
+	void player (const char *, const char *);
 	void playerStop ();
+
+	void playFile (const char *file, const char *next_file);
 
 private:
 	QDir pluginsDir;
@@ -72,6 +105,8 @@ private:
 
 	void playerReset ();
 	char *plistGetFile (int);
+
+	Precache precache;
 };
 
 #endif // _FOOAUDIOENGINE_HPP_
