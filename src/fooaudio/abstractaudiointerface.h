db87d3a355be1cc47347aa819983e4e7c1226647
Dariusz Mikulski <malcolm@sourcecode.pl>
2009-09-09 01:30:08 +0300

    Fix compilation

--- src/fooaudio/abstractaudiointerface.h
+++ src/fooaudio/abstractaudiointerface.h
@@ -24,12 +24,37 @@
 
 #include <QObject>
 
+class QUrl;
+
 namespace FooAudio
 {
-    class FAEXPORTS AbstractAudioInterface : public QObject
+    class FAEXPORTS AbstractAudioInterface
     {
-        Q_OBJECT
+    public:
+        virtual ~AbstractAudioInterface() {}
 
+        virtual bool isPlaying() = 0;
+        virtual bool isStopped() = 0;
+        virtual bool isPaused() = 0;
+        virtual bool isMuted() = 0;
+        virtual void setMuted(bool) = 0;
+
+        virtual qint64 totalTime() = 0;
+        virtual void seek(qint64 time) = 0;
+
+    signals:
+        void aboutToFinish();
+        void progress(qint64 time);
+        void willPlayNow(QUrl file);
+
+    public slots:
+        virtual void stop() = 0;
+        virtual void play() = 0;
+        virtual void pause() = 0;
+        virtual void clearQueue() = 0;
+        virtual void enqueueNextFile(QUrl file) = 0;
+        virtual void playFile(QUrl file) = 0;
+        virtual void setVolume(int volume) = 0;
     };
 }
 
