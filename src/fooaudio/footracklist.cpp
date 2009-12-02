6ea32544db122bbd167a22989142ac570e1b2fbf
Bartlomiej Zimon <uzi18@o2.pl>
2009-12-02 02:09:05 +0100

    - footracklist clean up

--- src/fooaudio/footracklist.cpp
+++ src/fooaudio/footracklist.cpp
@@ -1,15 +1,9 @@
-#include <QObject>
-
 #include "footracklist.hpp"
 
 FooTrackList::FooTrackList(QString name)
 {
-	if (name.isNull())
-	{
+	if (name.isEmpty())
 		Name = QObject::tr("New Playlist");
-	}
 	else
-	{
 		Name = name;
-	}
 }
