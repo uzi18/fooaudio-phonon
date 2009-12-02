8bea13786bea1a2c09464865a88d69b19f4f836c
Bartlomiej Zimon <uzi18@o2.pl>
2009-12-02 02:10:36 +0100

    - simple tracklist model

--- src/fooaudio/tracklistmodel.cpp
+++ src/fooaudio/tracklistmodel.cpp
@@ -1,5 +1,82 @@
+#include "footrack.hpp"
+#include "footracklist.hpp"
+
 #include "tracklistmodel.hpp"
 
-TrackListModel::TrackListModel()
+TrackListModel::TrackListModel(FooTrackList *tracklist, QObject *parent)
+	: QAbstractItemModel(parent), TrackList(tracklist)
 {
+}
+
+TrackListModel::~TrackListModel()
+{
+}
+
+int TrackListModel::columnCount(const QModelIndex &parent) const
+{
+	return 1;
+}
+
+int TrackListModel::rowCount(const QModelIndex &parent) const
+{
+	return TrackList->count();
+}
+
+QVariant TrackListModel::data(const QModelIndex &index, int role) const
+{
+	FooTrack trr = track(index);
+	if (trr.file().isEmpty())
+		return QVariant();
+
+	switch (role)
+	{
+		case Qt::DisplayRole:
+			if (index.column() == 0) // long or shor name?
+				return trr.file();
+			else
+				return QString("%1 (%2)").arg(trr.title(), trr.artist());
+
+// 		case Qt::DecorationRole:
+// 			return trr.icon().isNull()
+// 				? trr.icon()
+// 				: QVariant();
+// 
+// 		case TrackRole:
+// 			return QVariant::fromValue<FooTrack>(trr);
+
+		default:
+			return QVariant();
+	}
+}
+
+QVariant TrackListModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+	if (role != Qt::DisplayRole)
+		return QVariant();
+
+	if (orientation == Qt::Horizontal)
+		return QString("Column %1").arg(section);
+	else
+		return QString("Row %1").arg(section);
+}
+
+FooTrack TrackListModel::track(const QModelIndex &index) const
+{
+	if (!index.isValid())
+		return FooTrack();
+
+	if (index.row() < 0 || index.row() >= rowCount())
+		return FooTrack();
+
+	return TrackList->at(index.row());
+}
+
+int TrackListModel::trackIndex(FooTrack track)
+{
+	return TrackList->indexOf(track);
+}
+
+QModelIndex TrackListModel::trackModelIndex(FooTrack track)
+{
+	return createIndex(trackIndex(track), 0, 0);
 }
--- src/fooaudio/tracklistmodel.hpp
+++ src/fooaudio/tracklistmodel.hpp
@@ -3,23 +3,40 @@
 
 #include <QtCore/QAbstractItemModel>
 
-class QModelIndex;
-
 const int FooRoles = 1000;
 
-const int FileRoles = FooRoles + 0;
-const int TitleRole = FooRoles + 1;
-const int ArtistRole = FooRoles + 2;
-const int YearRole = FooRoles + 3;
-const int AlbumRole = FooRoles + 4;
-const int TrackRole = FooRoles + 5;
+const int FooTrackRole = FooRoles + 1;
+const int FileRole = FooRoles + 2;
+const int TitleRole = FooRoles + 3;
+const int ArtistRole = FooRoles + 4;
+const int YearRole = FooRoles + 5;
+const int AlbumRole = FooRoles + 6;
+const int TrackRole = FooRoles + 7;
+
+class QModelIndex;
+
+class FooTrack;
+class FooTrackList;
 
 class TrackListModel : public QAbstractItemModel
 {
 	Q_OBJECT
+	
+	FooTrackList * TrackList;
 
 public:
-	TrackListModel();
+	TrackListModel(FooTrackList *tracklist = 0, QObject *parent = 0);
+	virtual ~TrackListModel();
+
+	virtual int columnCount(const QModelIndex &parent) const;
+	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
+
+	virtual QVariant data(const QModelIndex &index, int role) const;
+	virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+
+	FooTrack track(const QModelIndex &index) const;
+	int trackIndex(FooTrack track);
+	QModelIndex trackModelIndex(FooTrack track);
 };
 
 #endif // TRACKLISTMODEL_HPP
